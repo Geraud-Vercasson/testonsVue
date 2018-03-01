0a18e84c788f1e3c6007efd7ba4e73d6bb9c1dbb
geraud-vercasson <geraud.vercasson@gmail.com>
2018-03-01 17:07:00 +0100

    la machine

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png">q
     <ol>
-      <li>La machine 1 est {{machines.first.enabled? 'ACTIVE': 'EN PANNE'}}</li>
-      <li>La machine 2 est {{machines.second.enabled? 'ACTIVE': 'EN PANNE'}}</li>
+      <li v-for="(machine,index) in machines">
+        La machine nommée <span v-bind:class="{red: !machine.enabled}">{{machine.name}}</span> est <span v-bind:class="{red: !machine.enabled}">{{machine.enabled? 'ACTIVE': 'EN PANNE'}}</span>
+        <button class="btn btn-info" v-on:click="edit(index)">Modifier le statut</button>
+      </li>
     </ol>
+    <h2>Ajouter une machine</h2>
+    <div class="row justify-content-center">
+      <label for="nouvelleMachine" class="col-lg-1">Nom :</label>
+      <input class="form-control col-lg-4" type="text" id="nouvelleMachine" v-model="machineTemporaire.name">
+      <label for="nouvelleMachineEnabled" class="col-lg-1">Active?</label>
+      <input id="nouvelleMachineEnabled" type="checkbox" v-model="machineTemporaire.enabled">
+    </div>
+    <button v-on:click ="addToMachines" class="btn btn-success">Ajouter aux machines</button>
+    <h2>Supprimer une machine</h2>
+    <div class="row justify-content-center">
+      <select v-model="toRemove" id="toRemove" class="form-control col-lg-4">
+        <option v-for="(machine, index) in machines" :value="index">{{machine.name}}</option>
+      </select>
+      <button class="btn btn-danger" v-on:click="remove(toRemove)">Supprimer</button>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
+export default {
     name: 'app',
     data() {
       return {
         msg: 'Welcome to Your Vue.js App',
-        machines: {
-          first: {
+        machines: [
+          {
+            name: "Starbucks' shitty coffee",
             enabled: true
           },
-          second: {
+          {
+            name: "Grand-Mère est une voleuse",
             enabled: false
           }
+        ],
+        machineTemporaire: {
+          name:"",
+          enabled:false
+        },
+        toRemove: null,
+        test: true
+      }
+    },
+    methods: {
+      addToMachines(){
+        if(this.machineTemporaire.name != ""){
+          this.machines.push({name: this.machineTemporaire.name, enabled: this.machineTemporaire.enabled});
         }
+      },
+      remove(selected){
+        if(selected != null){
+          this.machines.splice(selected,1);
+        }
+      },
+      edit(selected){
+        this.machines[selected].enabled = !this.machines[selected].enabled
       }
     }
   }
@@ -48,11 +88,32 @@
   }
 
   li {
-    display: inline-block;
     margin: 0 10px;
   }
 
   a {
     color: firebrick;
+  }
+
+  span{
+    font-weight: bold;
+    color: green;
+  }
+
+  /* Bootstrap Toggle v2.2.2 corrections for Bootsrtap 4*/
+  .toggle-off {
+    box-shadow: inset 0 3px 5px rgba(0, 0, 0, .125);
+  }
+  .toggle.off {
+    border-color: rgba(0, 0, 0, .25);
+  }
+
+  .toggle-handle {
+    background-color: white;
+    border: thin rgba(0, 0, 0, .25) solid;
+  }
+
+  .red{
+    color: red;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,14 +4,5 @@ import App from './App.vue'
 let myVue = new Vue({
   el: '#app',
   render: h => h(App),
-  data: {
-    machines: {
-      first: {
-        enabled: true
-      },
-      second: {
-        enabled: false
-      }
-    }
-  }
-});
+})
+;
